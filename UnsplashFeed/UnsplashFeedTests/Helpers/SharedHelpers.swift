1d44b76355d455fe163a24cc25980298d56fc63d
Azizbek Asadov <azizbek.asadov.work@gmail.com>
2026-04-05 21:59:37 +0200

    Add valudation and securing proper flow of the `URLSessionHTTPClient` with various edge cases Add Shared Helpers for mock data to be reused in a set of isolated tests

--- UnsplashFeed/UnsplashFeedTests/Helpers/SharedHelpers.swift
+++ UnsplashFeed/UnsplashFeedTests/Helpers/SharedHelpers.swift
@@ -14,3 +14,15 @@ func anyURL() -> URL {
 func anyNSError() -> NSError {
     NSError(domain: "any error", code: -1)
 }
+
+func anyData() -> Data {
+    Data("any bytes".utf8)
+}
+
+func anyHTTPResponse(with statusCode: Int = 200) -> HTTPURLResponse {
+    HTTPURLResponse(url: anyURL(), statusCode: statusCode, httpVersion: nil, headerFields: nil)!
+}
+
+func anyURLResponse() -> URLResponse {
+    URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+}
